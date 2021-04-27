6b229bb4ae9e8170e6aaf52ebf952caeb23ab35d
sykzen <sykzen@hotmail.com>
2021-04-27 16:13:08 +0200

    set : readme

---
+++ getCordinate.py
@@ -0,0 +1,10 @@
+import pyautogui
+#pyautogui.moveTo(786,372)
+#pyautogui.click()
+pyautogui.displayMousePosition()
+
+#How to use
+#run the script
+# place your mouse in your target
+# get the width and height
+# replace it here Pyautogui.moveto(width,height)
